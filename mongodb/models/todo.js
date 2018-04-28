1f3a2affab890324235174560729f98319763faf
Arbaz Yousuf <contactme797@gmail.com>
2018-04-28 13:17:18 +0500

    NodeJs-Teach

---
+++ mongodb/models/todo.js
@@ -0,0 +1,21 @@
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const todoSchema = Schema({
+    todo:String
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+module.exports = Todo
+
+
+
+
+
+
+
+/* 
+todo: { type: String, required: true },
+    checked: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now } */
---
+++ mongodb/server.js
@@ -0,0 +1,30 @@
+// import modules
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+// initialize express app
+let app = express();
+const port = 3002;
+/**
+ * Here we are importing our routes;
+ */ 
+require("./mongodb");
+const routers = require("./routes.js");
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })) 
+// parse application/json
+app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Credentials', "true");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+app.use('/api',routers);
+// Starting server
+app.listen(port, () => {
+    console.log("server is running!!")
+});
+//
